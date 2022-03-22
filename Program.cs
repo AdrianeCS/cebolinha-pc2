60404c4262d337230fdae9976b5df955bf4acf16
adriane <adrianedacosta21@gmail.com>
2022-03-22 17:30:05 -0300

    termino da lição

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string fraseDigitada, fraseComDislalia;
+            
+Console.Write("Digite uma frase cebolinha: ");
+fraseDigitada = Console.ReadLine()!;
+fraseComDislalia = fraseDigitada!
+ .Replace("r", "l")
+.Replace("R", "L");
+Console.WriteLine(fraseComDislalia);
